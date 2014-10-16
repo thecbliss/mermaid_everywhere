5fedb7a0a5ad8278329dbc69fcdd5d2d82b9e96a
stanley zheng <dev.stanzheng@gmail.com>
2014-10-16 18:49:28 -0400

    relative tld

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ L.tileLayer(
   }).addTo(map);
 
  // load GeoJSON from an external file
-$.getJSON("../data/mermaid.geojson", function(data) {
+$.getJSON("/data/mermaid.geojson", function(data) {
   var ratIcon = L.icon({
     iconUrl: '../images/mermaid-icon.png',
     iconSize: [28, 28],
